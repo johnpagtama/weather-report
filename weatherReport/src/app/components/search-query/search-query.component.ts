83203a55360eb8ce140521a72560a1dd2ec4d887
johnpagtama <pagtamajr@gmail.com>
2021-02-04 19:04:16 -0800

    Created components

---
+++ weatherReport/src/app/components/search-query/search-query.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-query',
+  templateUrl: './search-query.component.html',
+  styleUrls: ['./search-query.component.scss']
+})
+export class SearchQueryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
